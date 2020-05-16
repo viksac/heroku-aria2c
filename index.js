2c018fa5d609754b8f73b7fea4104078df4e35c2
Aid3n_Pearce <61540835+Aid-3n@users.noreply.github.com>
2020-05-17 02:09:05 +0530

    Update index.js

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ app.get('/', (req, res) => {
 <label for="secret">Enter your aria2 secret:</label>
 <input id="secret" type="password">
 <button id="panel">Go to AriaNg panel</button>
-<button id="downloads">View downloaded files</button>
+<button id="downloads">You Should Come Back Here After Download Is Completed</button>
 <script>
 panel.onclick=function(){
 	open('/ariang/#!/settings/rpc/set/wss/'+location.hostname+'/443/jsonrpc/'+btoa(secret.value),'_blank')
